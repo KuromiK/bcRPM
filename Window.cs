d627a35e99611889643fc21d921a760207445029
KuromiK <98142015+KuromiK@users.noreply.github.com>
2023-01-24 10:43:25 +0300

    Update Window.cs

--- Window.cs
+++ Window.cs
@@ -1,6 +1,6 @@
 ﻿namespace WinFormsLibrary1;
 
-public abstract class Window : Event
+public abstract class Window
 {
     public void postEvent(Event ev) { }
     public abstract void processEvent(Event ev);
